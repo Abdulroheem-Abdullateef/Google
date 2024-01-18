711579ba86402fde9db3fe6f2f6bc92aacb00483
Yuslaw <yusufadetola07@gmail.com>
2024-01-18 22:45:01 +0100

    Add project files.

---
+++ Controllers/LocationController.cs
@@ -0,0 +1,25 @@
+﻿using Google.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+
+public class LocationController : Controller
+{
+    public IActionResult GetLocation()
+    {
+        // Implement logic to retrieve locations from a database or other source
+        var locations = new List<Location>
+        {
+            new Location { Name = "Location 1", Latitude = 37.7749, Longitude = -122.4194 },
+            new Location { Name = "Location 2", Latitude = 34.0522, Longitude = -118.2437 },
+            // Add more locations as needed
+        };
+
+        return View(locations);
+    }
+
+    [HttpPost] // Handle post requests (button click)
+    public IActionResult ShowLocations()
+    {
+        return RedirectToAction("Index"); // Redirect to the Index action (showing locations)
+    }
+}
---
+++ Models/Location.cs
@@ -0,0 +1,10 @@
+﻿namespace Google.Models
+{
+    public class Location
+    {
+        public string Name { get; set; }
+        public double Latitude { get; set; }
+        public double Longitude { get; set; }
+    }
+
+}
